df7f387df08d80512380689b78ff680cd3978c50
Vidya <vidya30@gmail.com>
2013-07-17 21:23:31 -0700

    Changed web.js again

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
+var fs = require('fs');      
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var tempBuffer = fs.readFileSync('index.html', 'utf8');
+//var buf= new Buffer(tempBuffer.length, 'utf8');
+
+console.log(tempBuffer.toString());
+//console.log(buf.toString('utf8', 0, tempBuffer.length));
 app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
